b55b4718e423111948a02c1ff5fa7341d31ab973
Mykola <mykola.dziuba92@gmail.com>
2026-01-05 12:13:45 +0100

    fix AZ errors

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,30 +1,35 @@
 // infra/variables.tf
 variable "aws_region" {
-  type    = string
-  default = "eu-central-1"
+  type        = string
+  description = "AWS region"
+  default     = "eu-central-1"
 }
 
 variable "prefix" {
-  type    = string
-  default = "mykola"
+  type        = string
+  description = "Prefix for all resources"
+  default     = "mykola"
 }
 
 variable "image" {
   type        = string
-  description = "Full ECR image URI with tag, e.g. 123.dkr.ecr.eu-central-1.amazonaws.com/ip-test-app:1.0"
+  description = "Full ECR image URI, e.g. <account>.dkr.ecr.eu-central-1.amazonaws.com/ip-test-app:1.0"
 }
 
 variable "cpu" {
-  type    = string
-  default = "256"
+  type        = string
+  description = "Fargate CPU units"
+  default     = "256"
 }
 
 variable "memory" {
-  type    = string
-  default = "512"
+  type        = string
+  description = "Fargate memory (MiB)"
+  default     = "512"
 }
 
 variable "desired_count" {
-  type    = number
-  default = 1
+  type        = number
+  description = "Number of tasks"
+  default     = 1
 }
